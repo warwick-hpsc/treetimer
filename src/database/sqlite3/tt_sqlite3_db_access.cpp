47672b1c8cb5c5113b7a4453af9cba33f823d12f
Andrew Owenson <aowenson@protonmail.ch>
2021-04-30 22:26:36 +0100

    Move SQL updates to in-memory DB, for performance

--- src/database/sqlite3/tt_sqlite3_db_access.cpp
+++ src/database/sqlite3/tt_sqlite3_db_access.cpp
@@ -10,6 +10,8 @@
  *
  */
 
+#include <sys/stat.h>
+
 #include "tt_sqlite3_db_access.h"
 
 namespace treetimer
@@ -32,15 +34,79 @@ namespace treetimer
 			{
 				void openConnection(TTSQLite3& dataAccess)
 				{
-					// Open a Database Connection
-					int err = sqlite3_open(dataAccess.filePath.c_str(), &(dataAccess.db));
+					// Create in-memory DB. If DB file exists, copy that into memory
+
+					int err;
+					sqlite3* db_file = NULL;
+					sqlite3_backup* db_backup = NULL;
+
+					err = sqlite3_open(":memory:", &(dataAccess.db));
+					if (err != SQLITE_OK) {
+						printf("TreeTimer error: sqlite3_open(\":memory:\") failed"); fflush(stdout);
+						exit(EXIT_FAILURE);
+					}
+
+					bool db_file_exists = false;
+					struct stat buffer;
+					db_file_exists = (stat (dataAccess.filePath.c_str(), &buffer) == 0); 
+					if (db_file_exists) {
+						// Load and backup into in-memory DB:
+						err = sqlite3_open(dataAccess.filePath.c_str(), &db_file);
+						if (err != SQLITE_OK) {
+							printf("TreeTimer error: sqlite3_open(%s) failed", dataAccess.filePath.c_str()); fflush(stdout);
+							exit(EXIT_FAILURE);
+						}
+
+						db_backup = sqlite3_backup_init(dataAccess.db, "main", db_file, "main");
+						if (db_backup) {
+							sqlite3_backup_step(db_backup, -1);
+							sqlite3_backup_finish(db_backup);
+						}
+
+						err = sqlite3_close(db_file);
+						if (err != SQLITE_OK) {
+							printf("TreeTimer error: sqlite3_close() of file DB failed"); fflush(stdout);
+							exit(EXIT_FAILURE);
+						}
+					}
+
 					sqlite3_exec(dataAccess.db,"BEGIN TRANSACTION",NULL,NULL,NULL);
 				}
 
 				void closeConnection(TTSQLite3& dataAccess)
 				{
+					// Write in-memory DB to file
+
+					int err;
+					sqlite3* db_file = NULL;
+					sqlite3_backup* db_backup = NULL;
+
 					sqlite3_exec(dataAccess.db,"END TRANSACTION",NULL,NULL,NULL);
-					int err = sqlite3_close(dataAccess.db);
+
+					err = sqlite3_open(dataAccess.filePath.c_str(), &db_file);
+					if (err != SQLITE_OK) {
+						printf("TreeTimer error: sqlite3_open(%s) failed", dataAccess.filePath.c_str()); fflush(stdout);
+						exit(EXIT_FAILURE);
+					}
+
+					db_backup = sqlite3_backup_init(db_file, "main", dataAccess.db, "main");
+					if (db_backup) {
+						sqlite3_backup_step(db_backup, -1);
+						sqlite3_backup_finish(db_backup);
+					}
+
+					err = sqlite3_close(db_file);
+					if (err != SQLITE_OK) {
+						printf("TreeTimer error: sqlite3_close() of file DB failed"); fflush(stdout);
+						exit(EXIT_FAILURE);
+					}
+
+					err = sqlite3_close(dataAccess.db);
+					if (err != SQLITE_OK) {
+						printf("TreeTimer error: sqlite3_close() of in-memory failed"); fflush(stdout);
+						exit(EXIT_FAILURE);
+					}
+					dataAccess.db = NULL;
 				}
 			}
 		}
