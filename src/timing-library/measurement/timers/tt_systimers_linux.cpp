5ebea76c37932151000445247e368e95b28f90f8
Andrew Owenson <a.owenson@warwick.ac.uk>
2021-12-19 22:41:10 +0000

    Fix PMTM license

--- src/timing-library/measurement/timers/tt_systimers_linux.cpp
+++ src/timing-library/measurement/timers/tt_systimers_linux.cpp
@@ -1,11 +1,3 @@
-/*
- * File:   tt_systimers_linux.cpp
- * Author: AWE Plc (Tessella plc)
- *
- * Implementation of set_timers which gets the CPU and Elapsed time on systems
- * running the GNU Linux operating system.
- */
-
 #include "tt_systimers_linux.h"
 
 namespace treetimers
